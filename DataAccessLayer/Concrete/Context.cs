8a1c338ab2993c51220f799131226b5ff9d66ec8
Eda Bezek <eda_bezek@hotmail.com>
2022-06-14 09:47:37 +0300

    first migration

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=(LocalDB)\MSSQLLocalDB;Database=DynamicBlogDB;integrated security=true;");
+         }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
