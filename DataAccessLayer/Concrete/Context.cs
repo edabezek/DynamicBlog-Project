f740a56627ec80a6744c6e84857c5f8d42cf2b54
Eda Bezek <eda_bezek@hotmail.com>
2022-07-10 17:08:34 +0300

    Sign up to our newsletter part 37-38

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -21,6 +21,8 @@ namespace DataAccessLayer.Concrete
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<Writer> Writers { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<NewsLetter> NewsLetters { get; set; }
+
 
     }
 }
