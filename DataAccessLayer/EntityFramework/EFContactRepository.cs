abc69b124b6db98d379308024a5dd04b3ffacaaa
Eda Bezek <eda_bezek@hotmail.com>
2022-07-29 11:33:29 +0300

    Contact Page Added

--- DataAccessLayer/EntityFramework/EFContactRepository.cs
+++ DataAccessLayer/EntityFramework/EFContactRepository.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.EntityFramework
 {
-    public class EFContectRepository : GenericRepository<Contact>, IContactDal
+    public class EFContactRepository : GenericRepository<Contact>, IContactDal
     {
     }
 }
