d9da91701974bd51727514d531e78900c38e1bc6
eda.bezek <eda_bezek@hotmail.com>
2022-08-16 14:15:56 +0300

    dashboard statistics added

--- DynamicBlogProject/Controllers/DashboardController.cs
+++ DynamicBlogProject/Controllers/DashboardController.cs
@@ -1,7 +1,9 @@
 ﻿using BusinnesLayer.Concrete;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace DynamicBlogProject.Controllers
 {
@@ -11,7 +13,10 @@ namespace DynamicBlogProject.Controllers
         [AllowAnonymous]
         public IActionResult Index()//BlogListDashboard ye gider
         {
-
+            Context context = new Context();
+            ViewBag.v1 = context.Articles.Count().ToString();//Total Number of Blogs
+            ViewBag.v2=context.Articles.Where(x=>x.WriterId==1).Count().ToString();//Total Number of My Blogs
+            ViewBag.v3=context.Categories.Count().ToString();//Total Number of Category
             return View();
         }
     }
