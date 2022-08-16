3f4315c2febc852a32da88323bd145757d34bf13
eda.bezek <eda_bezek@hotmail.com>
2022-08-16 10:45:53 +0300

    blogRating added -67

---
+++ EntityLayer/Concrete/BlogRating.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public  class BlogRating//blogların değerlendirmesini burada tutacağız 
+    {
+        public int BlogRatingId { get; set; }
+        public int BlogId { get; set; }
+        public int BlogTotalScore { get; set; }
+        public int BlogRatingCount{ get; set; }//kaç oy verildiği ortalama yapılması için 
+
+    }
+}
